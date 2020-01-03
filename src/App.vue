3f8ff751223a36e5641411456ab01fb0a4e348a7
Julian Lehrhuber <jl@lerry.de>
2020-01-03 15:20:19 +0100

    Initial commit of launcher

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  components: {},
+  beforeMount() {
+    this.$vs.theme({
+      primary: this.$store.state.primaryColor
+    });
+
+    this.$store.subscribe(mutation => {
+      if (mutation.type == "primaryColor") {
+        this.$vs.theme({
+          primary: mutation.payload
+        });
+      }
+    });
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import Vuesax from "vuesax";
+import Vuex from "vuex";
 
-Vue.config.productionTip = false
+// Components
+import App from "./App.vue";
+
+// Styles
+import "vuesax/dist/vuesax.css"; //Vuesax styles
+
+Vue.use(Vuesax, {
+  // options
+});
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    primaryColor: localStorage.getItem("primaryColor") || "rgb(0,0,0)"
+  },
+  mutations: {
+    primaryColor(state, color) {
+      localStorage.setItem("primaryColor", color);
+    }
+  }
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  store,
+  render: h => h(App)
+}).$mount("#app");
