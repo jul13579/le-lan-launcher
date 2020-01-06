fc177b121c650fa3ab1eecb8cae74b26c296d0fe
Julian Lehrhuber <jl@lerry.de>
2020-01-06 14:08:47 +0100

    Fancy-up the game lib

--- src/components/Games.vue
+++ src/components/Games.vue
@@ -176,6 +176,10 @@ export default {
     display: inline-block
     width: 150px
     margin: 10px
+    cursor: pointer
+    transition: box-shadow .2s ease-in-out;
+    &:hover
+      box-shadow: 0px 0px 20px 5px white
     img
       width: 100%
       height: auto
