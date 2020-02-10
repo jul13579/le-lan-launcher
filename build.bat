3973842d75fb179c72a68541709d0692750b254d
Julian Lehrhuber <jl@lerry.de>
2020-02-10 10:10:36 +0100

    Add syncthing patches and build script

---
+++ build.bat
@@ -0,0 +1,37 @@
+@echo off
+if exist syncthing goto build
+:cloneSyncthing
+echo Cloning Syncthing...
+git clone git@github.com:syncthing/syncthing.git
+
+:build
+echo.
+echo Pulling latest changes...
+pushd syncthing
+git reset --hard
+git pull origin master
+
+echo.
+echo Checking out latest syncthing release tag (v1.3.4)
+git checkout v1.3.4
+
+echo.
+echo Patching syncthing
+for %%f in (..\patches\*) do (
+    git apply %%f
+    echo Applied %%f
+)
+
+echo.
+echo Building Syncthing...
+go run build.go -version v1.3.4-LEGC -no-upgrade build
+popd
+
+echo.
+echo Move binary into place...
+move syncthing\syncthing* .\
+
+echo.
+echo Building LEGC LAN-Launcher...
+npm run electron:build
+exit 0
