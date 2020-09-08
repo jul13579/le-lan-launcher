8662505475d51612afd684da7a49ee5f50c361c4
Julian Lehrhuber <jl@lerry.de>
2020-09-08 13:02:49 +0200

    WIP Begin replacing vuesax with vuetify

--- src/background.js
+++ src/background.js
@@ -41,7 +41,10 @@ async function createWindow() {
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
-    if (!process.env.IS_TEST) win.webContents.openDevTools();
+    if (!process.env.IS_TEST) {
+      win.maximize();
+      win.webContents.openDevTools();
+    }
   } else {
     createProtocol("app");
     // Load the index.html when not in development
--- src/main.js
+++ src/main.js
@@ -1,67 +1,71 @@
 import Vue from "vue";
-import Vuesax from "vuesax";
+// import Vuesax from "vuesax";
 import Toasted from "vue-toasted";
 import VueI18n from "vue-i18n";
+
+import vuetify from "@/plugins/vuetify";
 
 // Components
 import App from "./App.vue";
 
 import store from "./store";
-import langs from "./langs.js"
+import langs from "./langs.js";
 
 // Styles
-import "material-icons/iconfont/material-icons.css";
-import "./stylus/vuesax.styl";
+// import "material-icons/iconfont/material-icons.css";
+// import "./stylus/vuesax.styl";
+import "vuetify/dist/vuetify.min.css";
 import "./sass/app.sass";
 
-Vue.use(Vuesax, {
-  theme: {
-    colors: {
-      primary: "#FFF"
-      // success:'rgb(23, 201, 100)',
-      // danger:'rgb(242, 19, 93)',
-      // warning:'rgb(255, 130, 0)',
-      // dark:'rgb(36, 33, 69)'
-    }
-  }
-});
+// Vue.use(Vuesax, {
+//   theme: {
+//     colors: {
+//       primary: "#FFF",
+//       // success:'rgb(23, 201, 100)',
+//       // danger:'rgb(242, 19, 93)',
+//       // warning:'rgb(255, 130, 0)',
+//       // dark:'rgb(36, 33, 69)'
+//     },
+//   },
+// });
 Vue.use(Toasted, {
   position: "bottom-center",
-  duration: 5000
+  duration: 5000,
 });
 Vue.use(VueI18n);
 
 // Lets Register a Global Error Notification Toast.
 Vue.toasted.register(
   "success",
-  message => {
+  (message) => {
     return message;
   },
   {
     type: "success",
-    icon: "check"
+    icon: "check",
   }
 );
 Vue.toasted.register(
   "error",
-  message => {
+  (message) => {
     return message;
   },
   {
     type: "error",
-    icon: "error_outline"
+    icon: "error_outline",
   }
 );
 
 const i18n = new VueI18n({
   locale: store.state.locale, // set locale
-  messages: langs // set locale messages
+  messages: langs, // set locale messages
 });
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   store,
   i18n,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
