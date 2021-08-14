d3e03346bcb38e008c24881e773d52a3c1e435d6
Julian Lehrhuber <jl@lerry.de>
2021-08-14 14:42:42 +0200

    Cleanup `SyncServiceMainController`

--- src/controllers/SyncServiceMainController.js
+++ src/controllers/SyncServiceMainController.js
@@ -3,8 +3,6 @@ import path from "path";
 import parse from "xml-parser";
 import fs from "fs";
 
-let syncServiceProcess;
-
 export default class SyncServiceMainController {
   static start(win, homeDir) {
     if (homeDir) {
@@ -15,36 +13,36 @@ export default class SyncServiceMainController {
         args.push("-no-console");
       }
 
-      syncServiceProcess = spawn(binPath, args, {});
+      this.syncServiceProcess = spawn(binPath, args, {});
 
-      syncServiceProcess.stdout.on("data", (data) => {
+      this.syncServiceProcess.stdout.on("data", (data) => {
         win.webContents.send("syncService", {
           type: "stdout",
           message: `${data}`,
         });
       });
-      syncServiceProcess.stderr.on("data", (data) => {
+      this.syncServiceProcess.stderr.on("data", (data) => {
         win.webContents.send("syncService", {
           type: "stderr",
           message: `${data}`,
         });
       });
-      syncServiceProcess.on("exit", (code) => {
+      this.syncServiceProcess.on("exit", (code) => {
         win.webContents.send("syncService", {
           type: "stdout",
           message: `Process exited with exit code ${code}`,
         });
       });
     }
-  };
+  }
   static stop() {
-    return syncServiceProcess.kill("SIGTERM");
-  };
+    return this.syncServiceProcess.kill("SIGTERM");
+  }
   static async getApiKey(homeDir) {
     const xml = parse(
-      fs.readFileSync(path.join(homeDir, "config.xml"), "utf8")
+      fs.readFileSync(path.join(homeDir, "config.xml"), { encoding: "utf8" })
     );
     const gui = xml.root.children.find((item) => item.name == "gui");
     return gui.children.find((item) => item.name == "apikey").content;
-  };
-};
+  }
+}
