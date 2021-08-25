877a008f92e25644d6e881d234bccad62c3c891b
Julian Lehrhuber <jl@lerry.de>
2021-08-25 14:21:55 +0200

    Remove locale definition from vuetify plugin
    The app does not use any localized strings of vuetify, so this was
    removed

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,14 +1,9 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
-import de from "vuetify/es5/locale/de"; // TODO: import other locales and switch!
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-  lang: {
-    locales: { de },
-    current: "de",
-  },
   theme: {
     dark: true,
   },
