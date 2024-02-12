fe3d6c52377f3659a3be80c751b6ede0e732edc8
Julian Lehrhuber <jl@lerry.de>
2024-02-12 14:46:06 +0100

    Remove comment pointing to `electron-builder`

--- src/main.ts
+++ src/main.ts
@@ -42,8 +42,6 @@ async function createWindow() {
     webPreferences: {
       webSecurity: true,
       preload: path.join(__dirname, "preload.js"),
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: !!process.env.ELECTRON_NODE_INTEGRATION,
       contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
     },
