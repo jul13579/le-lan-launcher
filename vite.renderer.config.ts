1d2ed85c432b783ecdea9e2486b3755d427116df
Julian Lehrhuber <jl@lerry.de>
2024-02-12 14:37:51 +0100

    Add `@vitejs/vue-plugin`

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  plugins: [vue()],
+});
