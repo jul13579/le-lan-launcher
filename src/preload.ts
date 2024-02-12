a720956d77f768f29bb024c4e6de72df57c0a221
Julian Lehrhuber <jl@lerry.de>
2024-02-12 15:03:21 +0100

    Fix linting problems in `preload.ts`

--- src/preload.ts
+++ src/preload.ts
@@ -5,20 +5,20 @@ import { contextBridge, ipcRenderer } from "electron";
 contextBridge.exposeInMainWorld("ipcRenderer", {
   send: (channel, ...data) => {
     // whitelist channels
-    let validChannels = ["controlWindow", "controlLibrary", "setProgress"];
+    const validChannels = ["controlWindow", "controlLibrary", "setProgress"];
     if (validChannels.includes(channel)) {
       ipcRenderer.send(channel, ...data);
     }
   },
   invoke: (channel, ...data) => {
     // whitelist channels
-    let validChannels = ["showOpenDialog", "controlSyncService", "controlGame"];
+    const validChannels = ["showOpenDialog", "controlSyncService", "controlGame"];
     if (validChannels.includes(channel)) {
       return ipcRenderer.invoke(channel, ...data);
     }
   },
   on: (channel, func) => {
-    let validChannels = ["syncService", "setApiKey", "library", "game"];
+    const validChannels = ["syncService", "setApiKey", "library", "game"];
     if (validChannels.includes(channel)) {
       // Deliberately strip event as it includes `sender`
       ipcRenderer.on(channel, func);
