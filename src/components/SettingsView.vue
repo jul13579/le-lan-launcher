d6618c9ce70347428719cb907fc796224342ffe0
Julian Lehrhuber <jl@lerry.de>
2024-02-12 22:53:00 +0100

    Remove all `require` statements

--- src/components/SettingsView.vue
+++ src/components/SettingsView.vue
@@ -15,27 +15,27 @@
 
 <template>
   <v-container>
-    <v-alert type="error" text border="left" :title="$t('errors.playerNameUnset.title')" v-if="playerName == false">
+    <v-alert type="error" border="start" :title="$t('errors.playerNameUnset.title')" v-if="playerName == false">
       {{ $t("errors.playerNameUnset.message") }}
     </v-alert>
-    <v-alert type="error" text border="left" :title="$t('errors.homeDirUnset.title')" v-if="homeDir == false">
+    <v-alert type="error" border="start" :title="$t('errors.homeDirUnset.title')" v-if="homeDir == false">
       {{ $t("errors.homeDirUnset.message") }}
     </v-alert>
-    <v-alert type="error" text border="left" :title="$t('errors.nasUnset.title')" v-if="nas == false">
+    <v-alert type="error" border="start" :title="$t('errors.nasUnset.title')" v-if="nas == false">
       {{ $t("errors.nasUnset.message") }}
     </v-alert>
     <div class="text-h4">{{ $t("settings.theme") }}</div>
     <div class="d-flex mx-n3 flex-wrap">
       <v-img v-for="(item, index) in textures" :key="index" class="themePreview ma-3" aspect-ratio="1" :src="item"
         @click.native="() => {
-          store.commit(require('../enums/Mutations').default.THEME, { path: item, cover: false });
+          store.commit(Mutations.THEME, { path: item, cover: false });
         }
           " eager></v-img>
       <div class="themePreview ma-3 bg-transparent-dark align-center justify-center"
         :style="{ border: `1px solid hsl(${backgroundHue}, 100%, 35%)` }" @click="
           openFileChooser(
             (result) =>
-              store.commit(require('../enums/Mutations').default.THEME, {
+              store.commit(Mutations.THEME, {
                 path: `theme://${result.filePaths[0].replace(/\\/g, '/')}`,
                 cover: true,
               }),
@@ -60,7 +60,7 @@
       <v-col cols="12">
         <v-slider :min="0" :max="360" thumb-label v-model="sliderValue" :color="'hsl(' + sliderValue + ', 100%, 50%)'"
           @change="(input) => {
-            store.commit(require('../enums/Mutations').default.BACKGROUND_HUE, input);
+            store.commit(Mutations.BACKGROUND_HUE, input);
           }
             " />
       </v-col>
@@ -71,7 +71,7 @@
       <v-col cols="3">
         <v-select :label="$t('settings.language')" :value="locale" :items="Object.entries(langs)"
           :item-text="(lang) => lang[1].lang" :item-value="(lang) => lang[0]"
-          @change="(input) => store.commit(require('../enums/Mutations').default.LOCALE, input)" />
+          @change="(input) => store.commit(Mutations.LOCALE, input)" />
       </v-col>
       <v-col cols="5" class="offset-4">
         <v-tooltip top max-width="400">
@@ -89,7 +89,7 @@
     <v-row>
       <v-col cols="3">
         <v-text-field :label="$t('settings.playerName')" :value="playerName" @blur="(event) => {
-          store.commit(require('../enums/Mutations').default.PLAYER_NAME, event.target.value);
+          store.commit(Mutations.PLAYER_NAME, event.target.value);
         }
           " :error="playerName == false" />
       </v-col>
@@ -103,7 +103,7 @@
       <v-col cols="5">
         <v-select :disabled="!online" :label="$t('settings.nas')" :value="nas" :items="Object.entries(devices)"
           :item-value="(device) => device[0]" :item-text="(device) => device[0]" :error="nas == false"
-          @change="(input) => store.commit(require('../enums/Mutations').default.NAS, input)"
+          @change="(input) => store.commit(Mutations.NAS, input)"
           :no-data-text="$t('settings.alerts.discovery')"
           :error-messages="!online ? $t('settings.alerts.service') : null" />
       </v-col>
@@ -192,7 +192,7 @@ function openFileChooser(callback: (result: any) => void, options: any) {
 function setHomeDir() {
   openFileChooser(
     (result) => {
-      store.commit(require('../enums/Mutations').default.HOME_DIR, result.filePaths[0]);
+      store.commit(Mutations.HOME_DIR, result.filePaths[0]);
     },
     { properties: ['openDirectory'] }
   )
