c60e087503215674174a3f978c76343f862ccb32
Julian Lehrhuber <jl@lerry.de>
2024-07-27 17:16:10 +0200

    Add basic `App` component definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
 import { createRoot } from "react-dom/client";
+
+function App() {
+  return <h1>Hello World</h1>;
+}
+
 const root = createRoot(document.body);
-root.render(<h1>Hello World</h1>);
+root.render(<App />);
