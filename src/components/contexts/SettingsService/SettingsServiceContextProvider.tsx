63d94f0b05708007fc107c1c8ae6e38888d3acf7
Julian Lehrhuber <jl@lerry.de>
2024-07-27 18:05:32 +0200

    Add draft `SettingsServiceContextProvider` implementation

--- src/components/contexts/SettingsService/SettingsServiceContextProvider.tsx
+++ src/components/contexts/SettingsService/SettingsServiceContextProvider.tsx
@@ -1,5 +1,6 @@
-import { FunctionComponent, ReactNode } from "react";
+import { FunctionComponent, ReactNode, useState } from "react";
 import { SettingsServiceContext } from "./SettingsServiceContext";
+import { defaultBackgroundHue, defaultTheme } from "src/config/app";
 
 interface SettingsServiceContextProviderProps {
   children: ReactNode;
@@ -9,9 +10,41 @@ export const SettingsServiceContextProvider: FunctionComponent<
   SettingsServiceContextProviderProps
 > = ({ children }) => {
   /* -------------------------------------------------------------------------- */
+  /*                                    State                                   */
+  /* -------------------------------------------------------------------------- */
+  const [backgroundHue, setBackgroundHue] = useState(defaultBackgroundHue);
+  const [theme, setTheme] = useState({
+    cover: false,
+    path: defaultTheme,
+  });
+  const [playerName, setPlayerName] = useState("");
+  const [homeDir, setHomeDir] = useState("");
+  const [apiKey, setApiKey] = useState("");
+  const [nas, setNas] = useState("");
+  const [locale, setLocale] = useState("");
+  const [debug, setDebug] = useState(false);
+
+  /* -------------------------------------------------------------------------- */
   /*                                  Rendering                                 */
   /* -------------------------------------------------------------------------- */
-  const state = {};
+  const state = {
+    backgroundHue,
+    setBackgroundHue,
+    theme,
+    setTheme,
+    playerName,
+    setPlayerName,
+    homeDir,
+    setHomeDir,
+    apiKey,
+    setApiKey,
+    nas,
+    setNas,
+    locale,
+    setLocale,
+    debug,
+    setDebug,
+  };
   return (
     <SettingsServiceContext.Provider value={state}>
       {children}
---
+++ src/config/app.ts
@@ -0,0 +1,3 @@
+export const defaultBackgroundHue = 265;
+export const defaultLocale = "en";
+export const defaultTheme = "./funky-lines.png";
