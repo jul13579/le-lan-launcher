b9ba7be4026397d6106f51fa87d7385cffd85c18
Julian Lehrhuber <jl@lerry.de>
2024-07-27 15:55:14 +0200

    Uncomment Vue frontend initialization

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,37 +1,37 @@
-import Vue from "vue";
-import Toasted from "vue-toasted";
-import VueI18n from "vue-i18n";
+// import Vue from "vue";
+// import Toasted from "vue-toasted";
+// import VueI18n from "vue-i18n";
 
-import vuetify from "./plugins/vuetify";
+// import vuetify from "./plugins/vuetify";
 
-// Components
-import App from "./App.vue";
+// // Components
+// import App from "./App.vue";
 
-import store from "./store";
-import langs from "./localization/langs";
-import numberFormats from "./localization/numberformats";
+// import store from "./store";
+// import langs from "./localization/langs";
+// import numberFormats from "./localization/numberformats";
 
-// Styles
-import "roboto-fontface/css/roboto/roboto-fontface.css";
-import "./sass/app.scss";
+// // Styles
+// import "roboto-fontface/css/roboto/roboto-fontface.css";
+// import "./sass/app.scss";
 
-Vue.use(Toasted, {
-  position: "bottom-center",
-  duration: 5000,
-});
-Vue.use(VueI18n);
+// Vue.use(Toasted, {
+//   position: "bottom-center",
+//   duration: 5000,
+// });
+// Vue.use(VueI18n);
 
-const i18n = new VueI18n({
-  locale: store.state.locale, // set locale
-  numberFormats,
-  messages: langs, // set locale messages
-});
+// const i18n = new VueI18n({
+//   locale: store.state.locale, // set locale
+//   numberFormats,
+//   messages: langs, // set locale messages
+// });
 
-Vue.config.productionTip = false;
+// Vue.config.productionTip = false;
 
-new Vue({
-  vuetify,
-  store,
-  i18n,
-  render: (h) => h(App),
-}).$mount("#app");
+// new Vue({
+//   vuetify,
+//   store,
+//   i18n,
+//   render: (h) => h(App),
+// }).$mount("#app");
