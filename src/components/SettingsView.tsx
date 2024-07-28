7ee5b65224ead22e04cb57af9f45407ff001b30f
Julian Lehrhuber <jl@lerry.de>
2024-07-28 22:17:27 +0200

    Add alerts

--- src/components/SettingsView.tsx
+++ src/components/SettingsView.tsx
@@ -1,5 +1,31 @@
+import { Alert, Box, Container } from "@mui/material";
 import { FunctionComponent } from "react";
+import { useTranslation } from "react-i18next";
+import { useSettingsService } from "../hooks/useSettingsService";
 
 export const SettingsView: FunctionComponent = () => {
-  return <></>;
+  /* -------------------------------------------------------------------------- */
+  /*                                   Context                                  */
+  /* -------------------------------------------------------------------------- */
+  const { t } = useTranslation();
+  const { playerName, homeDir, nas } = useSettingsService();
+
+  /* -------------------------------------------------------------------------- */
+  /*                                  Rendering                                 */
+  /* -------------------------------------------------------------------------- */
+  return (
+    <Container>
+      {[
+        [!playerName, t("errors.playerNameUnset.message")],
+        [!homeDir, t("errors.homeDirUnset.message")],
+        [!nas, t("errors.nasUnset.message")],
+      ]
+        .filter(([visible]) => !!visible)
+        .map(([, text]) => (
+          <Box m={1}>
+            <Alert severity="error">{text}</Alert>
+          </Box>
+        ))}
+    </Container>
+  );
 };
