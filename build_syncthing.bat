686fae8a3d13c12bf0cba7f97c12b4984b94ddd6
Julian Lehrhuber <jl@lerry.de>
2024-08-02 11:11:40 +0200

    Upgrade Syncthing to 1.27.9

--- build_syncthing.bat
+++ build_syncthing.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set SYNCTHING_DIR="syncthing-src"
-set VERSION="v1.27.7"
+set VERSION="v1.27.9"
 
 if exist %SYNCTHING_DIR% goto build
 :cloneSyncthing
--- build_syncthing.sh
+++ build_syncthing.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SYNCTHING_DIR="syncthing-src"
-VERSION="v1.27.7"
+VERSION="v1.27.9"
 
 if [ ! -d "$SYNCTHING_DIR" ]; then
     echo "Cloning Syncthing..."
