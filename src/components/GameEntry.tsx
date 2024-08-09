dbf78fe29966335194bf7d5da52356024395f336
Julian Lehrhuber <jl@lerry.de>
2024-08-09 12:55:17 +0200

    Fix game thumbnail styling

--- src/components/GameEntry.tsx
+++ src/components/GameEntry.tsx
@@ -20,21 +20,21 @@ const hoverAnimation = "0.2s ease-in-out";
 
 interface GameEntryRootProps {
   installed: boolean;
+  thumbnail: string;
 }
 
 const GameEntryRoot = styled("div")<GameEntryRootProps>(
-  ({ theme, installed }) => ({
+  ({ theme, installed, thumbnail }) => ({
     position: "relative",
     width: "190px",
+    height: `${190 * 1.5}px`,
     overflow: "hidden",
     margin: theme.spacing(1.5),
     // Game image styling
-    img: {
-      width: "100%",
-      height: "auto",
-    },
+    backgroundImage: `url(${thumbnail})`,
+    backgroundSize: "cover",
     // Every child element that is not the game thumbnail is absolutely positioned => stacked on each other
-    "& > *:not(img)": {
+    "& > *": {
       position: "absolute",
       top: 0,
       left: 0,
@@ -63,7 +63,7 @@ interface ProgressIndicatorProps {
 const ProgressIndicator = styled("div")<ProgressIndicatorProps>(
   ({ downloadProgress }) => ({
     background: "rgba(0, 0, 0, 0.7)",
-    top: `${-downloadProgress * 100}%`,
+    top: `${-downloadProgress * 100}% !important`,
   })
 );
 
@@ -143,15 +143,16 @@ export const GameEntry: FunctionComponent<GameEntryProps> = ({
   /* -------------------------------------------------------------------------- */
   return (
     <>
-      <GameEntryRoot installed={installed}>
-        {/* Game Thumbnail */}
-        <img src={`game://${libFolderPath}/${gameConfig.cover}`} />
+      <GameEntryRoot
+        installed={installed}
+        thumbnail={`game://${libFolderPath}/${gameConfig.cover}`}
+      >
         {/* Progress indicator */}
         <ProgressIndicator downloadProgress={downloadProgress} />
         {/* Download buttons overlay. Only displayed when downloadProgress < 1, hence not completed */}
         {downloadProgress < 1 ? (
           <DownloadButtonsContainer>
-            {subscribed && Object.keys(thisGameFolderStatus).length === 0 ? (
+            {subscribed && thisGameFolderStatus ? (
               // If game is subscribed but there is no syncFolderStatus yet, show loader
               <Fab>
                 <TextColoredCircularProgress size={30} />
