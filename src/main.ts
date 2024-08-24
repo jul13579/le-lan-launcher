0a9c851f396b39c7ccebce3309fffed00471a1c7
Julian Lehrhuber <jl@lerry.de>
2024-08-24 20:39:29 +0200

    Remove MacOS-specific `activate` handler

--- src/main.ts
+++ src/main.ts
@@ -73,12 +73,6 @@ app.on("window-all-closed", () => {
   shutdown();
 });
 
-app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
-});
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
